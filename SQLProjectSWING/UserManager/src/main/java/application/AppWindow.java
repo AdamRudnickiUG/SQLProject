c2ca23cea63c3bcfe1957c37526417c9c614cac9
Adam Rudnicki <adam.rudnicki@poczta.onet.pl>
2020-05-20 19:16:00 +0200

    Menu dziala, przyciski na brudno w glownej czesci okna - teraz trzeba podpiac sql

--- SQLProjectSWING/UserManager/src/main/java/application/AppWindow.java
+++ SQLProjectSWING/UserManager/src/main/java/application/AppWindow.java
@@ -27,15 +27,21 @@ public class AppWindow extends JFrame {
         //ADDITION OF TOP PANEL
         getContentPane().add(makeTopPanel(), BorderLayout.NORTH);
 
+//        topPanel.add(makeDefaultButton("Edytuj kody pocztowe"));
+//        topPanel.add(makeDefaultButton("Edytuj uzytowników"));
+//        topPanel.add(makeDefaultButton("Edytuj kontakt_do_kontrahenta"));
 
-        //CREATION AND ADDITION OF MAIN HBOX
 
-//        Box mainHBox = Box.createHorizontalBox();
-//        mainHBox.setOpaque(true);
-//        mainHBox.setBackground(Color.blue);
-//
-//        getContentPane().add(mainHBox, BorderLayout.CENTER);
-//        mainHBox.add(makeDefaultButton("exampleButton"));
+        //CREATION AND ADDITION OF MAIN VBOX
+
+        Box mainVBox = Box.createVerticalBox();
+        mainVBox.setOpaque(true);
+        mainVBox.setBackground(Color.blue);
+
+        getContentPane().add(mainVBox, BorderLayout.CENTER);
+        mainVBox.add(makeDefaultButton("Edytuj kody pocztowe"));
+        mainVBox.add(makeDefaultButton("Edytuj uzytowników"));
+        mainVBox.add(makeDefaultButton("Edytuj kontakt_do_kontrahenta"));
 
 
         //MENU CREATION
@@ -53,7 +59,7 @@ public class AppWindow extends JFrame {
 
         //
         JPanel middlePanel = new JPanel();
-        middlePanel.add(testMenu);
+//        middlePanel.add(testMenu);
 
 //        getContentPane().add(testMenu, BorderLayout.SOUTH);
 //        mainHBox.add(testMenu);
@@ -67,31 +73,33 @@ public class AppWindow extends JFrame {
         setVisible(true);
     }
 
+    public JMenuBar defaultMenu(){
+        JMenuBar tempMenuBar = new JMenuBar();
+
+        JMenu tempMenu = new JMenu("Menu");
+
+        JMenuItem item1 = new JMenuItem("example item 1");
+        JMenuItem item2 = new JMenuItem("example item 2");
+        JMenuItem item3 = new JMenuItem("example item 3");
+
+        tempMenu.add(item1);
+        tempMenu.add(item2);
+        tempMenu.add(item3);
+
+        tempMenuBar.add(tempMenu);
+
+        return tempMenuBar;
+    }
+
     private JPanel makeTopPanel() {
         topPanel = new JPanel();
         UiUtils.minMaxPrefHeight(topPanel, GlobalSizes.menuHeight);
         UiUtils.border(topPanel, Color.lightGray, 0, 0, 1, 0);
 //        topPanel.add(makeTmpButton());
-
-
-        topPanel.add(makeDefaultButton("Edytuj kody pocztowe"));
-        topPanel.add(makeDefaultButton("Edytuj uzytowników"));
-        topPanel.add(makeDefaultButton("Edytuj kontakt_do_kontrahenta"));
+        topPanel.add(defaultMenu());
         return topPanel;
     }
 
-    private JButton makeTmpButton() {
-        JButton butExampleDialog = new JButton();
-        UiUtils.minMaxPref(butExampleDialog, GlobalSizes.buttonWidth, GlobalSizes.buttonHeight);
-        butExampleDialog.setText("Example dialog");
-
-        butExampleDialog.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent actionEvent) {
-                AppGraph.getInstance().getJustDialog().setVisible(true);
-            }
-        });
-        return butExampleDialog;
-    }
 
     private JButton makeDefaultButton(String displayedText) {
         MyButton tempButton = new MyButton(GlobalSizes.buttonHeight, GlobalSizes.buttonWidth);
